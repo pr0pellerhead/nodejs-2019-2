a2160d88a1522a13d4e4be26b119c5c64fcfd788
IvaPivkova <pivkova_iva@hotmail.com>
2019-04-17 22:05:04 +0200

    jwt

--- c5/handlers/auth.js
+++ c5/handlers/auth.js
@@ -2,6 +2,7 @@ var userModel = require('../models/auth');
 var userValidator = require('../validators/auth');
 var validator = require('node-input-validator');
 var bcrypt = require('bcrypt-nodejs');
+var jwt = require('jsonwebtoken');
 
 var register = (req, res) => {
     var v = new validator(req.body, userValidator.addUser);
@@ -45,7 +46,18 @@ var login = (req, res) => {
         if(data){
             bcrypt.compare(req.body.password, data.password, function(err, r) {
                 if(r){
-                    res.status(200).send('ok');
+                    // Send JWT ...
+                    var exp = new Date();
+                    var token = jwt.sign(
+                        {
+                            uid: data._id,
+                            name: data.full_name,
+                            email: data.email,
+                            exp: parseInt(exp.getTime() / 1000 + 60)
+                        },
+                        'perocarnajboljiusvet'
+                    );
+                    res.status(200).send(token);
                 } else {
                     res.status(400).send('Bad Request');
                 }
@@ -60,7 +72,27 @@ var login = (req, res) => {
     });
 }
 
+var token = (req, res) => {
+    res.status(200).send(req.user);
+}
+
+var renew = (req, res) => {
+    var exp = new Date();
+    var token = jwt.sign(
+        {
+            uid: req.user.uid,
+            full_name: req.user.full_name,
+            email: req.user.email,
+            exp: parseInt(exp.getTime() / 1000 + 600)
+        },
+        'perocarnajboljiusvet'
+    );
+    res.status(200).send(token);
+}
+
 module.exports = {
     register,
-    login
+    login,
+    token,
+    renew
 };
--- c5/index.js
+++ c5/index.js
@@ -2,14 +2,31 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongo = require('./db/mongo');
 var handlers = require('./handlers/auth');
+var jwt = require('express-jwt');
 
 mongo.init();
 
 var api = express();
 api.use(bodyParser.json());
+api.use(
+    jwt(
+        {secret: 'perocarnajboljiusvet'}
+    )
+    .unless(
+        {path: ['/auth/register', '/auth/login']}
+    )
+);
 
 api.post('/auth/register', handlers.register);
 api.post('/auth/login', handlers.login);
+api.post('/auth/token', handlers.token);
+api.get('/auth/renew', handlers.renew);
+
+api.use(function (err, req, res, next) {
+    if(err.name === 'UnauthorizedError') {
+        res.status(401).send('Unauthorized');
+    }
+});
 
 api.listen(8080, err => {
     if(err){
